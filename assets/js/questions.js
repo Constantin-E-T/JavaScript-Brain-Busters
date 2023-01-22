751842e2a2ab8e7adb4c68b26357889558e70019
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-01-22 13:11:34 +0200

    added an array of objects for question js file

---
+++ assets/js/questions.js
@@ -0,0 +1,41 @@
+// TODO In your questions.js file, create an array of objects that contain the questions and multiple-choice answers. and the right answer. ✅
+
+
+
+let questionsArray = [ 
+    {        
+     question: "What is a closure in JavaScript?", 
+     choices: ["A function that returns another function", "A function that takes no arguments", "A function that is executed immediately", "A function that can only be called once"],
+     answer: "A function that returns another function"
+   },
+   {
+       question: "What is the difference between let and var in JavaScript?",
+       choices: ["let is block-scoped and var is function-scoped", "let can be reassigned, but var cannot", "var is deprecated and should not be used", "let is only used for loops and var is used for everything else"],
+       answer: "let is block-scoped and var is function-scoped"
+   },
+   {
+       question: "What is the difference between == and === in JavaScript?",
+       choices: ["== checks for equality and === checks for identity", "== checks for type coercion and === does not", "=== is faster than ==", "== is only used for numbers and === is used for everything else"],
+       answer: "== checks for equality and === checks for identity"
+   },
+   {
+       question: "What is the difference between a forEach loop and a for loop in JavaScript?",
+       choices: ["forEach is used for arrays and for is used for objects", "forEach can only be used on arrays and for can be used on any data type", "forEach is faster than for", "forEach only iterates through the keys of an object and for iterates through the values"],
+       answer: "forEach can only be used on arrays and for can be used on any data type"
+   },
+   {
+       question: "What is hoisting in JavaScript?",
+       choices: ["The process of moving variable and function declarations to the top of their scope", "The process of moving variable and function expressions to the bottom of their scope", "The process of creating a new variable or function", "The process of removing unnecessary variables and functions"],
+       answer: "The process of moving variable and function declarations to the top of their scope"
+   }
+];
+
+
+// print how many questions there are
+// console.log(questions.length);
+
+ 
+
+
+
+
